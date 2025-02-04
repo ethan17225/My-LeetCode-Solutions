e81c7ec0af662184993a5521625c4223a7e45021
Hai Nguyen <ethannguyen17225@gmail.com>
2025-02-03 22:55:25 -0500

    Time: 0 ms (100%), Space: 18 MB (9.06%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
@@ -1,31 +1,15 @@
 class Solution:
     def maxAscendingSum(self, nums: List[int]) -> int:
         max_sum = 0
-        temp = 0
+        temp = nums[0]
         n = len(nums)
-        i = 0
+        
+        for i in range(1, n):
+            if nums[i] <= nums[i-1]:
+                max_sum = max(max_sum, temp)
+                temp = 0
 
-        if n == 1:
-            return nums[0]
+            temp += nums[i]
 
-        while i < n:
-            if i == n-1:
-                if nums[i] > nums[i-1]:
-                    temp += nums[i]
-
-                return max_sum if temp <= max_sum else temp
-
-            if nums[i] >= nums[i+1]:
-                temp += nums[i]
-                i += 1
-
-                if temp > max_sum:
-                    max_sum = temp
-
-                temp = 0    
-
-            else:
-                temp += nums[i]
-                i += 1
-
-        return max_sum
+        return max(max_sum, temp)
+            
