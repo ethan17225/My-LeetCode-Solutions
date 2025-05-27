0948b2d4722d733c82e31c530736e52ae43e270a
Hai Nguyen <ethannguyen17225@gmail.com>
2025-05-27 12:53:04 -0400

    Time: 3 ms (60.78%), Space: 17.7 MB (67.05%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
@@ -6,17 +6,17 @@ class Solution:
         if m == n:
             return sum([x for x in range(1, n)]) - n
 
-        total = non_divisible_total = 0
+        total = divisible_total = 0
         i = 1
 
         while i <= n:
             total += i
-            if i % m != 0:
-                non_divisible_total += i
+            if i % m == 0:
+                divisible_total += i
 
             i += 1
 
-        return non_divisible_total - (total - non_divisible_total)
+        return (total - divisible_total) - divisible_total
 
     
 
