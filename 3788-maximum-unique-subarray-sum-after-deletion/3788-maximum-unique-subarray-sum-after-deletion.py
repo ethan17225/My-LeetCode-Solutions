630f2ad2605eda6ee3c4675c0773febe844c4988
Hai Nguyen <ethannguyen17225@gmail.com>
2025-07-25 12:38:29 -0400

    Time: 0 ms (100%), Space: 17.7 MB (51.76%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        uniqueNums = set(nums)
+        maxSum = 0
+        smallestNum = 0
+
+        for num in uniqueNums:
+            if num > 0:
+                maxSum += num
+
+        return maxSum if maxSum > 0 else max(uniqueNums)
